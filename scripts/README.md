b82cf953ed536b5b87f4b9c6a99078049551d4f9
John Deck <jdeck88@gmail.com>
2018-03-17 07:23:19 -0700

    add cpad geojson to output, running runCpad.sh to populate the output directory

--- scripts/README.md
+++ scripts/README.md
@@ -7,7 +7,8 @@ which reads the contents of package.json
 npm install
 ```
 
-To run the runCpad.js script to harvest CPAD data as geojson, type the following to get command-line help:
+To run the runCpad.js script to harvest CPAD data as geojson, type the following to get command-line help.
+The output of this command will place data into a cpad directory off of root (../cpad/)
 
 ```
 % node runCpad.js
--- scripts/runCpad.sh
+++ scripts/runCpad.sh
@@ -3,3 +3,11 @@
 # this script writes output that will be stored in the bnhm/spatial-layers
 # repository
 # TODO: insert the actual directories to process here
+node ./runCpad.js State California+Department+of+Parks+and+Recreation
+node ./runCpad.js State California+Department+of+Forestry+and+Fire+Protection
+node ./runCpad.js State California+Department+of+Fish+and+Wildlife
+node ./runCpad.js Special+District East+Bay+Regional+Park+District
+node ./runCpad.js Federal United+States+National+Park+Service
+node ./runCpad.js Federal United+States+Forest+Service
+node ./runCpad.js Federal United+States+Bureau+of+Land+Management
+node ./runCpad.js Federal United+States+Army+Corps+of+Engineers
